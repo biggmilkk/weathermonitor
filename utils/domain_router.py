adb9199782c3cdc33872ee72f76d7e2b1bd049eb
biggmilkk <scents-thermal9r@icloud.com>
2025-07-10 13:01:50 +0800

    Update domain_router.py

--- utils/domain_router.py
+++ utils/domain_router.py
@@ -1,9 +1,7 @@
-from scraper import weather_gov
+from scraper import nws_rss
 
 SCRAPER_MAP = {
-    "www.weather.gov": weather_gov.scrape,
-    # Add more domains as needed
+    "www.weather.gov/rss_page.php": nws_rss.scrape,
+    "www.weather.gov": nws_rss.scrape,  # optional fallback
 }
 
-def get_scraper(domain):
-    return SCRAPER_MAP.get(domain)
