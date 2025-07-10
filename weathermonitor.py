7800996f34c5cb1dd46d0d577e1906d64bb8f419
biggmilkk <scents-thermal9r@icloud.com>
2025-07-10 15:55:09 +0800

    Update weathermonitor.py

--- weathermonitor.py
+++ weathermonitor.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os
 import sys
 import json
+import logging
+logging.basicConfig(level=logging.WARNING)
 
 # Extend import path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
