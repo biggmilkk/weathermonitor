b22e469543b9e6e3e4d7ad2044dda2343dfd0a26
biggmilkk <scents-thermal9r@icloud.com>
2025-07-10 11:18:30 +0800

    Update domain_router.py

--- utils/domain_router.py
+++ utils/domain_router.py
@@ -1,1 +1,9 @@
+from scrapers import weather_gov
 
+SCRAPER_MAP = {
+    "www.weather.gov": weather_gov.scrape,
+    # Add more domains as needed
+}
+
+def get_scraper(domain):
+    return SCRAPER_MAP.get(domain)
