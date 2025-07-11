cbefe488a0f7799b7ed471120446b385c24762d3
biggmilkk <scents-thermal9r@icloud.com>
2025-07-11 23:18:38 +0800

    Delete utils/domain_router.py

--- utils/domain_router.py
+++
@@ -1,9 +0,0 @@
-from scraper import nws_active_alerts, environment_canada
-
-SCRAPER_MAP = {
-    "api.weather.gov": nws_active_alerts.scrape,
-    "weather.gc.ca": environment_canada.scrape_async
-}
-
-def get_scraper(domain):
-    return SCRAPER_MAP.get(domain)
