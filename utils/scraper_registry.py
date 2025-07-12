ba7ddc796e872e2bed2e4d55753a9ef3846f9a10
biggmilkk <scents-thermal9r@icloud.com>
2025-07-13 00:25:48 +0800

    Update scraper_registry.py

--- utils/scraper_registry.py
+++ utils/scraper_registry.py
@@ -3,10 +3,11 @@ import asyncio
 from scraper.environment_canada import scrape_ec
 from scraper.meteoalarm import scrape_meteoalarm
 from scraper.nws_active_alerts import scrape_nws
+from scraper.cma import scrape_cma
 
 SCRAPER_REGISTRY = {
     "json": lambda conf: scrape_nws(conf.get("url")),
     "ec_async": lambda conf: asyncio.run(scrape_ec(json.load(open(conf.get("source_file"))))),
     "rss_meteoalarm": lambda conf: scrape_meteoalarm(conf),
-    "rss_cma": lambda conf: asyncio.run(scrape_cma(conf)),
+    "rss_cma": lambda conf: scrape_cma(conf),
 }
