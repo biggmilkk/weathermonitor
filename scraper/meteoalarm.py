bdcec2bbbe2682282364f2a130e37eba9f4ca953
biggmilkk <scents-thermal9r@icloud.com>
2025-07-12 22:13:20 +0800

    Update meteoalarm.py

--- scraper/meteoalarm.py
+++ scraper/meteoalarm.py
@@ -90,13 +90,15 @@ def scrape_meteoalarm(conf):
 
                 if summary_today:
                     summary_lines.append("Today")
-                    summary_lines.extend(summary_today)
+                    summary_lines.extend([line for line in summary_today])
+                    summary_lines.append("")
 
                 if summary_tomorrow:
                     summary_lines.append("Tomorrow")
-                    summary_lines.extend(summary_tomorrow)
+                    summary_lines.extend([line for line in summary_tomorrow])
+                    summary_lines.append("")
 
-                summary_text = "\n".join(summary_lines)  # Ensure each alert is on its own line
+                summary_text = "\n".join(summary_lines) # Ensure each alert is on its own line
 
                 entries.append({
                     "title": f"{country} Alerts",
