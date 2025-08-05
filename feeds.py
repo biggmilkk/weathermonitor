189eb0b124f3a013bc57c912ef455f6c431904f7
biggmilkk <scents-thermal9r@icloud.com>
2025-08-05 15:35:02 +0800

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -20,4 +20,26 @@ def get_feed_definitions():
             "type": "rss_cma",
             "url": "https://severeweather.wmo.int/v2/cap-alerts/cn-cma-xx/rss.xml"
         },
+        "bom_all": {
+            "label": "Australia BOM",
+            "type": "rss_bom_multi",
+            "urls": [
+                "http://www.bom.gov.au/fwo/IDZ00054.warnings_nsw.xml",  # NSW & ACT
+                "http://www.bom.gov.au/fwo/IDZ00059.warnings_vic.xml",  # Victoria
+                "http://www.bom.gov.au/fwo/IDZ00056.warnings_qld.xml",  # Queensland
+                "http://www.bom.gov.au/fwo/IDZ00060.warnings_wa.xml",   # West Australia
+                "http://www.bom.gov.au/fwo/IDZ00057.warnings_sa.xml",   # South Australia
+                "http://www.bom.gov.au/fwo/IDZ00058.warnings_tas.xml",  # Tasmania
+                "http://www.bom.gov.au/fwo/IDZ00055.warnings_nt.xml",   # Northern Territory
+            ],
+            "states": [
+                "NSW & ACT",
+                "Victoria",
+                "Queensland",
+                "West Australia",
+                "South Australia",
+                "Tasmania",
+                "Northern Territory",
+            ],
+        },
     }
