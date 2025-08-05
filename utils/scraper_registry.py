d509845976b27faf63f5f70d7a8d2e5f76b4e490
biggmilkk <scents-thermal9r@icloud.com>
2025-08-05 15:37:01 +0800

    Update scraper_registry.py

--- utils/scraper_registry.py
+++ utils/scraper_registry.py
@@ -2,25 +2,28 @@ import json
 
 # Lazy-loading scraper functions to prevent circular imports
 SCRAPER_REGISTRY = {
-    # NWS: use full conf dict, not just URL string
+    # NWS active alerts
     "json": lambda conf, client: __import__(
         'scraper.nws_active_alerts', fromlist=['scrape_nws_async']
     ).scrape_nws_async(conf, client),
 
-    # Environment Canada: load sources JSON then async
+    # Environment Canada RSS feeds
     "ec_async": lambda conf, client: __import__(
         'scraper.environment_canada', fromlist=['scrape_ec_async']
-    ).scrape_ec_async(
-        json.load(open(conf.get('source_file'))), client
-    ),
+    ).scrape_ec_async(json.load(open(conf.get('source_file'))), client),
 
-    # MeteoAlarm: pass full conf
+    # MeteoAlarm countries
     "rss_meteoalarm": lambda conf, client: __import__(
         'scraper.meteoalarm', fromlist=['scrape_meteoalarm_async']
     ).scrape_meteoalarm_async(conf, client),
 
-    # CMA: pass full conf
+    # China Meteorological Admin regions
     "rss_cma": lambda conf, client: __import__(
         'scraper.cma', fromlist=['scrape_cma_async']
     ).scrape_cma_async(conf, client),
+
+    # BOM multi-state Australia
+    "rss_bom_multi": lambda conf, client: __import__(
+        'scraper.bom', fromlist=['scrape_bom_multi_async']
+    ).scrape_bom_multi_async(conf, client),
 }
