0affcb69abac350c96e88eca704676b78d99a4af
biggmilkk <scents-thermal9r@icloud.com>
2025-08-06 13:58:02 +0800

    Update scraper_registry.py

--- utils/scraper_registry.py
+++ utils/scraper_registry.py
@@ -26,4 +26,9 @@ SCRAPER_REGISTRY = {
     "rss_bom_multi": lambda conf, client: __import__(
         'scraper.bom', fromlist=['scrape_bom_multi_async']
     ).scrape_bom_multi_async(conf, client),
+
+    # JMA
+    "jma": lambda conf, client: __import__(
+        "scraper.jma", fromlist=["scrape_jma_async"]
+    ).scrape_jma_async(conf, client),
 }
