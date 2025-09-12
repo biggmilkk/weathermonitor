718d7abf66525b7262670bb3d6d65925748e295b
biggmilkk <scents-thermal9r@icloud.com>
2025-09-13 02:02:38 +0800

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -12,6 +12,13 @@ def get_feed_definitions():
             "type": "ec_async",
             "source_file": "environment_canada_sources.json",
         },
+        "meteoalarm": {
+            "label": "Meteoalarm (Europe)",
+            "type": "rss_meteoalarm",
+            "url": "https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-rss-europe",
+        },
+
+        # --- Met Office UK (REGIONAL) ---
         "metoffice_uk": {
             "label": "Met Office (UK)",
             "type": "uk_grouped_compact",
@@ -52,11 +59,6 @@ def get_feed_definitions():
                 "London & South East England",
             ],
         },
-        "meteoalarm": {
-            "label": "Meteoalarm (Europe)",
-            "type": "rss_meteoalarm",
-            "url": "https://feeds.meteoalarm.org/feeds/meteoalarm-legacy-rss-europe",
-        },
 
         "cma_china": {
             "label": "CMA (China)",
@@ -74,25 +76,24 @@ def get_feed_definitions():
                 "011000","012000","013000","014100","014030","015000","016000","017000",
                 "020000","030000","040000","050000","060000","070000","080000","090000",
                 "100000","110000","120000","130000","140000","190000","200000","210000",
-                "220000","230000","240000","150000","160000","170000","180000","250000",
-                "260000","270000","280000","290000","300000","310000","320000","330000",
-                "340000","360000","370000","380000","390000","350000","400000","410000",
-                "420000","430000","440000","450000","460100","460040","471000","472000",
-                "473000","474000"
+                "220000","230000","240000","250000","260000","270000","280000","290000",
+                "300000","310000","320000","330000","340000","350000","360000","370000",
+                "380000","390000","400000","410000","420000","430000","440000","460100",
+                "460400","470000",
             ],
-            "region_map_file": "scraper/region_area_codes.json",
         },
-        "bom_all": {
+
+        "bom_multi": {
             "label": "BOM (Australia)",
             "type": "rss_bom_multi",
             "urls": [
-                "https://www.bom.gov.au/fwo/IDZ00054.warnings_nsw.xml",  # NSW & ACT
-                "https://www.bom.gov.au/fwo/IDZ00059.warnings_vic.xml",  # Victoria
-                "https://www.bom.gov.au/fwo/IDZ00056.warnings_qld.xml",  # Queensland
-                "https://www.bom.gov.au/fwo/IDZ00060.warnings_wa.xml",   # Western Australia
-                "https://www.bom.gov.au/fwo/IDZ00057.warnings_sa.xml",   # South Australia
-                "https://www.bom.gov.au/fwo/IDZ00058.warnings_tas.xml",  # Tasmania
-                "https://www.bom.gov.au/fwo/IDZ00055.warnings_nt.xml",   # Northern Territory
+                "https://www.bom.gov.au/fwo/IDZ00059.warnings_nsw_act.xml",  # NSW & ACT
+                "https://www.bom.gov.au/fwo/IDZ00056.warnings_vic.xml",     # Victoria
+                "https://www.bom.gov.au/fwo/IDZ00054.warnings_qld.xml",     # Queensland
+                "https://www.bom.gov.au/fwo/IDZ00053.warnings_wa.xml",      # Western Australia
+                "https://www.bom.gov.au/fwo/IDZ00057.warnings_sa.xml",      # South Australia
+                "https://www.bom.gov.au/fwo/IDZ00058.warnings_tas.xml",     # Tasmania
+                "https://www.bom.gov.au/fwo/IDZ00055.warnings_nt.xml",      # Northern Territory
             ],
             "states": [
                 "NSW & ACT",
