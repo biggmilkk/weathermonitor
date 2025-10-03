5329e767be5f8693650ed7e5aec61dfbb9706d39
biggmilkk <scents-thermal9r@icloud.com>
2025-10-04 01:37:03 +0800

    Delete utils/clients.py

--- utils/clients.py
+++
@@ -1,8 +0,0 @@
-# utils/clients.py
-import httpx
-import streamlit as st
-
-@st.cache_resource
-def get_async_client():
-    """Singleton AsyncClient for all scrapers."""
-    return httpx.AsyncClient(timeout=30.0)
