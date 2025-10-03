4e8f421def7f23d2a3f9b0ce322802d6cbaf914f
biggmilkk <scents-thermal9r@icloud.com>
2025-10-04 03:39:43 +0800

    Update __init__.py

--- renderers/__init__.py
+++ renderers/__init__.py
@@ -2,11 +2,14 @@ from .nws import render as render_nws_grouped_compact
 from .ec import render as render_ec_grouped_compact
 from .uk import render as render_uk_grouped
 from .cma import render as render_cma
+from .meteoalarm import render as render_meteoalarm
+
 
 RENDERERS = {
     "nws_grouped_compact": render_nws_grouped_compact,
     "ec_grouped_compact": render_ec_grouped_compact,
     "uk_grouped_compact": render_uk_grouped,
     "rss_cma": render_cma,
+    "rss_meteoalarm": render_meteoalarm,
 
 }
