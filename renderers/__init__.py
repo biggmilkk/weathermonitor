0e998d89ca1dcf384a5edad8da74f339fae4c8f8
biggmilkk <scents-thermal9r@icloud.com>
2025-10-04 03:32:07 +0800

    Update __init__.py

--- renderers/__init__.py
+++ renderers/__init__.py
@@ -1,9 +1,11 @@
 from .nws import render as render_nws_grouped_compact
 from .ec import render as render_ec_grouped_compact
+from renderers.uk import render as render_uk_grouped
 
 
 RENDERERS = {
     "nws_grouped_compact": render_nws_grouped_compact,
     "ec_grouped_compact": render_ec_grouped_compact,
+    "uk_grouped_compact": render_uk_grouped,
 
 }
